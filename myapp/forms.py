05d0876ae0ee7bf0a0fc914bbea9e19f533557a6
Mantas An <mantasantanavic@gmail.com>
2025-07-19 18:45:54 +0300

    prideta projekto foto

--- myapp/forms.py
+++ myapp/forms.py
@@ -24,11 +24,12 @@ class PostForm(forms.ModelForm):
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'header_image']
 
         #widget = styling
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Posto pavadinimas'}), #we will pass css class, sita vieta bus naudojama bootsrapui formoms
             'content': forms.Textarea(attrs={'class': 'form-control'}), #we will pass css class sita vieta bus naudojama bootsrapui formoms
             #'author': forms.Select(attrs={'class': 'form-control'}), #we will pass css class sita vieta bus naudojama bootsrapui formoms
+            'header_image': forms.ClearableFileInput(attrs={'class': 'form-control'})
         }
